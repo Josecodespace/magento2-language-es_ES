63f2184977b4b4603e213659378f87d1ec6e80dc
Hennry Pilco <hensmau81@gmail.com>
2017-07-23 16:18:43 -0500

    Add files via upload

---
+++ registration.php
@@ -0,0 +1,12 @@
+<?php
+/**
+* @Autor: Ing. Hennry Pilco
+* @Alias: @hensx81
+* @Date: 23-07-2017
+* @Last Modified by: Hennry Pilco
+* @Last MOdified time: 23-07-2017
+* @website: http://sepia.solutions
+*/
+\Magento\Framework\Component\ComponentRegistrar::register( \Magento\Framework\Component\ComponentRegistrar::LANGUAGE, 'sepiaopensource_es_ES', __DIR__
+);
+?>
